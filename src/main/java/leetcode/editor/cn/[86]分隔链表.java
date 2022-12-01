b850bfdba84c954228b6aa2554255a34d8c306ac
haoshaofei <haoshaofei@kuaishou.com>
2022-12-01 18:12:22 +0800

    单链表:分割链表

---
+++ src/main/java/leetcode/editor/cn/[86]分隔链表.java
@@ -0,0 +1,80 @@
+package leetcode.editor.cn;
+import common.ListNode;
+//给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+//
+// 你应当 保留 两个分区中每个节点的初始相对位置。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,4,3,2,5,2], x = 3
+//输出：[1,2,2,4,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [2,1], x = 2
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 200] 内 
+// -100 <= Node.val <= 100 
+// -200 <= x <= 200 
+// 
+// Related Topics 链表 双指针 
+// 👍 651 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode partition(ListNode head, int x) {
+
+
+        // 存放小于x 的链表
+        ListNode dummy1 = new ListNode(-1);
+        // 存放大于 x  的链表
+        ListNode dummy2 = new ListNode(-1);
+        // p1 p2 指针
+        ListNode p1 = dummy1, p2 = dummy2;
+        ListNode p = head;
+        while (p != null) {
+            if (p.val < x) {
+                p1.next = p;
+                p1 = p1.next;
+            } else {
+                p2.next = p;
+                p2 = p2.next;
+            }
+            // 断开原链表中的每个节点
+            ListNode temp = p.next;
+            p.next = null;
+            p = temp;
+        }
+        // 合并两个链表
+        p1.next = dummy2.next;
+        return dummy1.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
