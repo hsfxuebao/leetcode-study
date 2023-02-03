e7d3c340e3e616da891122fa65644fae9bbc8dbd
haoshaofei <haoshaofei@kuaishou.com>
2023-02-03 09:56:39 +0800

    图-Prim最小生成树算法

--- src/main/java/leetcode/editor/cn/[1584]连接所有点的最小费用.java
+++ src/main/java/leetcode/editor/cn/[1584]连接所有点的最小费用.java
@@ -83,7 +83,118 @@ class P1584_MinCostToConnectAllPoints{
     }  
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int minCostConnectPoints(int[][] points) {
+
+        /**
+         * Prim最小生成树算法
+         */
+        public int minCostConnectPoints(int[][] points) {
+
+            List<int[]>[] graph = buildGraph(points.length, points);
+            Prim prim = new Prim(graph);
+            return prim.weightSum();
+        }
+
+        // 构造无向图
+        List<int[]>[] buildGraph(int n, int[][] points) {
+            List<int[]>[] graph = new LinkedList[n];
+            for (int i = 0; i < n; i++) {
+                graph[i] = new LinkedList<>();
+            }
+            // 生成所有边及权重
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    int xi = points[i][0], yi = points[i][1];
+                    int xj = points[j][0], yj = points[j][1];
+                    int weight = Math.abs(xi - xj) + Math.abs(yi - yj);
+                    // 用 points 中的索引表示坐标点
+                    graph[i].add(new int[]{i, j, weight});
+                    graph[j].add(new int[]{j, i, weight});
+                }
+            }
+            return graph;
+        }
+
+
+        class Prim {
+            // 核心数据结构，存储「横切边」的优先级队列
+            private PriorityQueue<int[]> pq;
+            // 类似visited数组，记录那个节点已成为最小生成树节点
+            private boolean[] inMst;
+            // 记录最小生成数的权重之和
+            int weightSum = 0;
+            // graph 是用邻接表表示的一幅图，
+            // graph[s] 记录节点 s 所有相邻的边，
+            // 三元组 int[]{from, to, weight} 表示一条边
+            private List<int[]>[] graph;
+
+            public Prim(List<int[]>[] graph) {
+                this.graph = graph;
+                // 按照边的权重从小到大排序
+                this.pq = new PriorityQueue<>((a,b) -> a[2]-b[2]);
+                int n = graph.length;
+                inMst = new boolean[n];
+
+                // 随便从一个节点开始切分，不防从节点0开始切分
+                inMst[0] = true;
+                cut(0);
+
+                while (!pq.isEmpty()) {
+
+                    int[] edge = pq.poll();
+                    int to = edge[1];
+                    int weight = edge[2];
+                    if (inMst[to]) {
+                        // 节点 to 已经在最小生成树中，跳过
+                        // 否则这条边会产生环
+                        continue;
+                    }
+                    // 将边 edge 加入最小生成树
+                    weightSum += weight;
+                    inMst[to] = true;
+                    // 节点 to 加入后，进行新一轮切分，会产生更多横切边
+                    cut(to);
+                }
+
+            }
+
+            // 将 s 的横向边  加入优先队列
+            private void cut(int s) {
+
+                // 遍历s 的邻边
+                for (int[] edge : graph[s]) {
+                    int to = edge[1];
+                    if (inMst[to]) {
+                        // 相邻接点 to 已经在最小生成树中，跳过
+                        // 否则这条边会产生环
+                        continue;
+                    }
+                    // 加入横切边队列
+                    pq.offer(edge);
+                }
+            }
+            // 最小生成树 权重之和
+            public int weightSum() {
+                return weightSum;
+            }
+            // 判断 最小生成树是否包含 图中的所有节点
+            public boolean allConnected() {
+                for (int i = 0; i < inMst.length; i++) {
+
+                    if (!inMst[i]) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+        }
+
+
+
+        /**
+         * Kruskal最小生成树算法
+         */
+        public int minCostConnectPoints1(int[][] points) {
         int n = points.length;
         List<int[]> edges = new ArrayList<>();
 
